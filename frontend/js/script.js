42f8d33216e5ab805166cf8d3d5db7cb8b55018d
Filipi <filipidelima2008@gmail.com>
2026-01-11 16:44:55 -0300

    Change server url

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -79,7 +79,7 @@ const handleLogin = (event) => {
     login.style.display = 'none'
     chat.style.display = 'flex'
 
-    websocket = new WebSocket('ws://localhost:8080')
+    websocket = new WebSocket('wss://chat-backend-idsp.onrender.com')
 
     websocket.onmessage = processMessage
 
